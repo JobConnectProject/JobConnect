3af237c0d1a3538879cd4327453dfa4302c966a2
dwng3 <answer3dw@gmail.com>
2024-05-25 17:03:11 +0900

    feat - 디렉토리, 엔티티 클래스 생성

--- BackEnd/src/main/java/com/jobconnect/backend/domain/Member.java
+++ BackEnd/src/main/java/com/jobconnect/backend/domain/Member.java
@@ -1,12 +1,15 @@
 package com.jobconnect.backend.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 
 @Entity
 @Getter
 public class Member {
 
+    @Id
     Long meId;
 
 
